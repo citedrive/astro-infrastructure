9508651e1938f702e01153998f6b8b67274fd723
garethbilaney <gareth.bilaney@hhu.de>
2022-09-04 09:58:24 +0200

    footer component

--- src/config.ts
+++ src/config.ts
@@ -42,22 +42,3 @@ export type Sidebar = Record<
 	typeof KNOWN_LANGUAGE_CODES[number],
 	Record<string, { text: string; link: string }[]>
 >;
-
-export const SIDEBARS: Sidebar = {
-	en: {
-		'Section Header': [
-			{ text: 'Introduction', link: 'en/introduction' },
-			{ text: 'Page 2', link: 'en/page-2' },
-			{ text: 'Page 3', link: 'en/page-3' },
-		],
-		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
-	},
-	fr: {
-		'Section Header': [
-			{ text: 'Introduction', link: 'fr/introduction' },
-			{ text: 'Page 2', link: 'fr/page-2' },
-			{ text: 'Page 3', link: 'fr/page-3' },
-		],
-		'Another Section': [{ text: 'Page 4', link: 'fr/page-4' }],
-	},
-};
