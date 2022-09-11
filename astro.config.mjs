7996eb81f1e3574d122315017efc3b8463586752
garethbilaney <gareth.bilaney@hhu.de>
2022-09-11 12:30:31 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://citedrive.github.io',
- base: '/astro-infrastructure',
   trailingSlash: 'never',
   integrations: [
     react()
