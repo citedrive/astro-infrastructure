18d37c1622afb621002da0695730feca68d007ae
garethbilaney <gareth.bilaney@hhu.de>
2023-02-15 14:08:21 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.citedrive.com',
+  //site: 'https://www.citedrive.com',
   trailingSlash: 'always',
   integrations: [react(), partytown(), mdx(), sitemap()],
   output: "server",
