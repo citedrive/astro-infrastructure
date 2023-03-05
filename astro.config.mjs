c18c6d191e38be397d6819e5a0ca8969bef667a7
garethbilaney <gareth.bilaney@hhu.de>
2023-03-05 14:15:47 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,10 @@ import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
  
   site: 'https://www.citedrive.com',
   trailingSlash: 'always',
   integrations: [react(), partytown(), mdx(), sitemap()],
-  /*
-  output: "server",
-
-  adapter: node({
-    mode: 'standalone'
-  }),
-  */
 });
