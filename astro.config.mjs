0d992ee4ccf997ea5c576f87abc4370e1145acbe
garethbilaney <gareth.bilaney@hhu.de>
2023-04-23 12:01:26 +0200

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
- 
   site: 'https://www.citedrive.com',
   trailingSlash: 'always',
-  integrations: [react(), partytown(), mdx(), sitemap()],
+  integrations: [react(), partytown(), mdx(), sitemap(), compress()]
 });
